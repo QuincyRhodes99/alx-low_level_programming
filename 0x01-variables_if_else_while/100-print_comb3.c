10e36eddba23e1cbdd4571c904bc17196bde2290
QuincyRhodes <iwehrhodes2016@gmail.com>
2022-04-10 10:00:31 -0700

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -11,7 +10,7 @@ int digit1, digit2;
 
 for (digit1 = 0; digit1 < 9; digit1++)
 {
-for (digit2 = digit1 = 1; digit2 < 10; digit2++)
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
