3334a30b3cbe960262972d0795f0ee8e93c62b48
QuincyRhodes <iwehrhodes2016@gmail.com>
2022-04-10 07:04:36 -0700

    my 10th task commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-int digit1, digit2;
+int 1, 2;
 
-for (digit1 = 0; digit1 < 9; digit1++)
+for (1 = 0; 1 < 9; 1++)
 {
-for (digit2 = digit1 = 1; digit2 < 10; digit2++)
+for (2 = 1 = 1; 2 < 10; 2++)
 {
-putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
+putchar((1 % 10) + '0');
+putchar((2 % 10) + '0');
 
-if (digit1 == 8 && digit2 == 9)
+if (1 == 8 && 2 == 9)
 continue;
 
 putchar(',');
