393276f71f7e798a047243b476816553f5fb288b
QuincyRhodes <iwehrhodes2016@gmail.com>
2022-04-14 04:54:35 -0700

    my first task commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
-
 /**
  * main - causes an infinite loop
+ *
  * Return: 0
  */
-
 int main(void)
 {
-    int i;
-    printf("Infinite loop incoming :(\n");
+int i;
 
-    i = o;
+printf("Infinite loop incoming :(\n");
 
-    /*while (i < 10)*/
-    /*{*/
-    /*putchar(1);*/
-    /*}*/
+i = 0;
 
-    printf(*Infinite loop avoided! \\o/\n");
+/*
+ *while (i < 10)
+ *{
+ *Infinite loop - No increase of variable
+ *putchar(i);
+}
+*/
 
-    return (0);
+printf("Infinite loop avoided! \\o/n");
+return (0);
 }
