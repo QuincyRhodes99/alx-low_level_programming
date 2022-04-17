80acb8bfcc2132261cce1a0db13be3fda33aec51
QuincyRhodes <iwehrhodes2016@gmail.com>
2022-04-17 09:21:28 -0700

    my third task commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_numbers - prints the number zero to nine
  *
@@ -8,14 +7,15 @@
 
 void print_numbers(void)
 {
-
 int i;
 
 i = 0;
-
-for (i = 0; i < 10; i++)
-
+while (i < 10)
+{
 _putchar(i + '0');
+i++;
+}
 
 _putchar ('\n');
 }
+
