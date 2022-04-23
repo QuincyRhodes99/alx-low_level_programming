19efd91cd0a22d2b8c6377f7b6d0f72a8b81a2f4
QuincyRhodes <iwehrhodes2016@gmail.com>
2022-04-23 10:17:49 -0700

    my third task commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,13 +3,13 @@
  * _strcmp - function that compares two strings
  * @s1: string to be compared
  * @s2: strng to be compared 
- * Return: negtive if s1 < s2, 0 if matching and positive int if s1 > s2
+ * Return: negtive if s1 < s2; 0 if matching and positive int if s1 > s2
  */
-int _strcmp(char *s1; char *s2)
+int _strcmp(char *s1, char *s2)
 {
 int x;
 
-for (x = 0; s1[x] != '\0' || s2[x] != '\0'; x++)
+for (x = 0; s1[x] != '\0'; || s2[x] != '\0'; x++)
 {
 if (s1[x] != s2[x])
 return (s1[x] = s2[x]);
