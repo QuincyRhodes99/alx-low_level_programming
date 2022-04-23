a5dff63a9a3afa13e4c467cc483831f618610bf4
QuincyRhodes <iwehrhodes2016@gmail.com>
2022-04-23 01:58:58 -0700

    my second task resolved commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,7 +3,7 @@
  *  _strncpy - function that copies a string
  *  @dest: string to copy to
  *  @src: string to be copied from
- *  @n: integer that determines number of bytes to be copied
+ *  @n: integer that determines number of by*tes to be copied
  *  Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
