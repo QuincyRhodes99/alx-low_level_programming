3f397e41ec4c15e781c16d5d33e247c6dcb4359e
QuincyRhodes <iwehrhodes2016@gmail.com>
2022-04-23 00:50:28 -0700

    my zero task resolved commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
  * _strcat - concatenates two strings,
  * @dest: destination.
@@ -7,20 +7,21 @@ nclude "main.h"
  */
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, count2 = 0;
+int count = 0, count2 = 0;
 
-	while (*(dest + count) != '\0')
-	{
-		count++;
-	}
-
-	while (count2 >= 0)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
-	}
-	return (dest);
+while (*(dest + count) != '\0')
+{
+count++;
 }
+
+while (count2 > = 0)
+{
+*(dest + count) = *(src + count2);
+if (*(src + count2) == '\0')
+break;
+count++;
+count2++;
+}
+return (dest);
+}
+
