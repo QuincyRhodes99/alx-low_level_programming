a24d950f3f83e082facab81c937fdf8617f9f66e
QuincyRhodes <iwehrhodes2016@gmail.com>
2022-04-24 08:13:47 -0700

    my task4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,11 @@
 void reverse_array(int *a, int n)
 {
 int x, y;
-
 for (x = 0; x < (n / 2); x++)
 {
 y = a[x];
-a[x] = a[n - x - l];
-a[n -x - l] =y;
+a[x] = a[n - x - 1];
+a[n -x - 1] =y;
 }
 }
 
