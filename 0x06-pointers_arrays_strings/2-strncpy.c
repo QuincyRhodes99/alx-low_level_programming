775e068344657cd65bb70079d292b90f60922964
QuincyRhodes <iwehrhodes2016@gmail.com>
2022-04-25 04:53:44 -0700

    my 2nd task commit resolved

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- *  _strncpy - copies a string
- *  @dest: destination.
- *  @src: source.
- *  @n: amount of bytes from src.
- *  Return: the pointer to dest.
+ * _strncpy - copies a string
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-int x;
+int i;
 
-for (x = 0; x < n && src[x] != '\0'; x++)
-dest[x] = src[x];
-for (; x < n; x++);
-dest[x] = '\0';
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
 
 return (dest);
 }
